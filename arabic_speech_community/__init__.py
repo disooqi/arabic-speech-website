e48b2b62302abbc0ddc616ae929732355534a06c
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2018-12-17 11:15:44 +0300

    Remove Email password and username from __init__.py

--- arabic_speech_community/__init__.py
+++ arabic_speech_community/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0447c95c364a0ae6200b1eee656c9053'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -20,10 +21,9 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = os.environ.get('ARABIC_SPEECH_EMAIL_USERNAME')
-# app.config['MAIL_PASSWORD'] = os.environ.get('ARABIC_SPEECH_EMAIL_PASSWORD')
-app.config['MAIL_USERNAME'] = 'arabicspeechinfo@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ArabicSpeech123'
+app.config['MAIL_USERNAME'] = os.environ.get('ARABIC_SPEECH_EMAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('ARABIC_SPEECH_EMAIL_PASSWORD')
+
 app.config['MAIL_DEFAULT_SENDER'] = 'arabicspeechinfo@gmail.com'
 info_mail = Mail(app)
 
