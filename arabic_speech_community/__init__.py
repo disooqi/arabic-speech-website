6c9cfb208a65c2ac980e35a349c5b416c93a47f1
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2018-12-21 15:34:54 +0300

    Change secret_key and DB_URI values and move them to the environment file

--- arabic_speech_community/__init__.py
+++ arabic_speech_community/__init__.py
@@ -7,8 +7,8 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '0447c95c364a0ae6200b1eee656c9053'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.environ.get('ARABIC_SPEECH_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('ARABIC_SPEECH_DB_URI')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -24,6 +24,8 @@ app.config['MAIL_USE_SSL'] = True
 # app.config['MAIL_USE_TLS'] = True
 # https://serverfault.com/questions/413397/how-to-set-environment-variable-in-systemd-service
 # https://askubuntu.com/questions/1071415/passing-environment-variables-to-systemd-service
+# https://serverfault.com/questions/868373/how-to-use-variables-in-a-systemd-service-file
+# https://serverfault.com/questions/868373/how-to-use-variables-in-a-systemd-service-file
 app.config['MAIL_USERNAME'] = os.environ.get('ARABIC_SPEECH_EMAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('ARABIC_SPEECH_EMAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'info@arabicspeech.org'
