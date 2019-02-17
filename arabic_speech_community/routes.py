00b5bcfe536162bc4e75f9c2a9dc16d99eba21c7
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2019-02-17 16:56:04 +0300

    [ISSUE#2] trail 6

--- arabic_speech_community/routes.py
+++ arabic_speech_community/routes.py
@@ -266,7 +266,8 @@ def mgb2_download(token):
     the_response = make_response()
     # the_response = make_response(send_from_directory('/data/mgb2', f'{mgb2_download_request.mgb2_part}.tar.bz2', as_attachment=True))
     # return send_from_directory('/data/mgb2', f'{mgb2_download_request.mgb2_part}.tar.bz2', as_attachment=True)
-
+    the_response.headers['Content-Disposition'] = f'attachment; filename={mgb2_download_request.mgb2_part}.tar.bz2'
+    the_response.headers['Content-Type'] = 'application/x-tar'
     the_response.headers['X-Accel-Redirect'] = f'/mgb2/download/{mgb2_download_request.mgb2_part}.tar.bz2'
 
 
