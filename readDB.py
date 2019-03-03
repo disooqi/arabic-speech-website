45b2cadafd52841cdefb01b81c67dfe432e0c934
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2019-03-03 06:20:45 +0300

    Read link status from database

--- readDB.py
+++ readDB.py
@@ -17,3 +17,25 @@ User.query.filter_by(username='ahmed').first()
 User.query.get(1)
 '''
 
+# Number of link requested for all three train, dev and test
+dwns = MGB2Link.query.all()
+n_link_created = len(dwns)
+
+# Number of dev link requested
+devs = MGB2Link.query.filter_by(mgb2_part='dev').all()
+n_dev_requested = len(devs)
+
+
+# Number of link requested by the user for all three train, dev and test
+usr1 = User.query.filter_by(email='disooqi@gmail.com').first()
+n_link_requested = len(usr1.mgb2_downloads)
+
+
+# Number of download trials  for a specific user
+usr1_total = 0
+for lnk in usr1.mgb2_downloads:
+	usr1_total += lnk.n_downloads
+else:
+    print(usr1_total)
+
+
