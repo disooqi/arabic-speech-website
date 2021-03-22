38d871efea83e9e58d2bebdd776cdccbc0b9963e
Mohamed Eldesouki <disooqi@gmail.com>
2021-03-23 00:31:38 +0200

    Update creatdb.py

--- creatdb.py
+++ creatdb.py
@@ -1,4 +1,21 @@
 #create DB
-from arabic_speech_community import db
-db.create_all()
+# NB! you need to set (export ARABIC_SPEECH_DB_URI="sqlite:///arabic_speech.db") in the .bashrc file in order for this
+# to run properly
+ from arabic_speech_community import db
+from arabic_speech_community.models import User, MGB2Link, Post
 
+ db.create_all()
+ 
+disooqi = User(fullname='Mohamed Eldesouki', email='disooqi@gmail.com', password='123', position='Research Associate',
+            affiliation='Qatar Computing Research Associate', department='ALT', address='QCRI/Research Complex/HBKU/QF',
+            telephone='974-33542583')
+
+mohamohamed = User(fullname='Mohamed Eldesouki', email='mohamohamed@qf.org.qa', password='123', position='Research Associate',
+            affiliation='Qatar Computing Research Associate', department='ALT', address='QCRI/Research Complex/HBKU/QF',
+            telephone='974-33542583')
+
+p1 = Post(title='my title', content='my content', user_id=disooqi.id)
+
+db.session.add(disooqi)
+db.session.add(mohamohamed)
+db.session.commit()
